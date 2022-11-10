173ade4dd825dba415abe63b64396e93b07ea95c
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2022-11-10 22:43:37 +0200

    Mrk 1

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,54 +1,57 @@
-import React, { Component } from 'react';
 import { Box } from 'components/Box';
 import { Statistics } from './Statistics';
 import { FeedbackOptions } from './FeedbackOptions';
 import { Section } from './Section';
 import { Notification } from './Notification';
+import { useState } from 'react';
 
-export class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const LeaveFeedback = e => {
+    switch (e.target.name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        console.log('Нет отзыва с такой оценкой');
+        break;
+    }
   };
 
-  LeaveFeedback = e => {
-    console.dir(e.target.name);
-    this.setState(prevState => {
-      return {
-        [e.target.name]: prevState[e.target.name] + 1,
-      };
-    });
-  };
+  const total = good + neutral + bad;
+  const positivePercentage = total === 0 ? 0 : Math.round((100 * good) / total);
 
-  render() {
-    const stats = this.state;
-    const total = stats.good + stats.neutral + stats.bad;
-    const positivePercentage =
-      total === 0 ? 0 : Math.round((100 * stats.good) / total);
-
-    return (
-      <Box>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.LeaveFeedback}
+  return (
+    <Box>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={LeaveFeedback}
+        />
+      </Section>
+      <Section title={'Statistics'}>
+        {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title={'Statistics'}>
-          {total === 0 ? (
-            <Notification message="There is no feedback" />
-          ) : (
-            <Statistics
-              good={stats.good}
-              neutral={stats.neutral}
-              bad={stats.bad}
-              total={total}
-              positivePercentage={positivePercentage}
-            />
-          )}
-        </Section>
-      </Box>
-    );
-  }
-}
+        )}
+      </Section>
+    </Box>
+  );
+};
